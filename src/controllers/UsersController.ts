a507213760f0ad1b9419e2f570bb8a0765fc107f
Leomssm <leonardomossim.redes@gmail.com>
2025-07-15 21:16:57 -0300

    Inclusão das rotas UPDATE e DELETE em UsersController.ts

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
 import { AppDataSource } from '../data-source';
 import { User } from '../entity/User';
-
+import { Not } from "typeorm";
 
 const router = express.Router();
 
@@ -39,7 +39,7 @@ router.get("/users/:id", async (req: Request, res: Response) => {
             return;
         }
 
-        res.status(200).json({user});
+        res.status(200).json({ user });
         return;
 
     } catch (error) {
@@ -82,6 +82,86 @@ router.post("/users", async (req: Request, res: Response) => {
         })
     }
 });
+
+router.put("/users/:id", async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const data = req.body;
+
+
+        const userRepository = AppDataSource.getRepository(User);
+
+        const user = await userRepository.findOneBy({ id: parseInt(id) });
+
+        if (!user) {
+            res.status(404).json({
+                message: "Usuário não encontrado!"
+            });
+            return;
+        }
+        // Verifica se existe outro usuário com o mesmo e-mail, ignorando o próprio
+        const existingUser = await userRepository.findOne({
+            where: {
+                email: data.email,
+                id: Not(parseInt(id))
+            }
+        });
+
+        if (existingUser) {
+            res.status(404).json({
+                message: "Usuário já existente com este e-mail!"
+            });
+            return;
+        }
+
+        //Atualizar os dados do usuário (user é o que o usuário já possui, e data é o que vem na requisição)
+        userRepository.merge(user, data);
+
+        //Salvando no banco de dados
+        const updateUser = await userRepository.save(user);
+
+        res.status(200).json({
+            message: "Usuário atualizado com sucesso!",
+            user: updateUser
+        })
+
+
+    } catch (error) {
+        res.status(500).json({
+            message: "Erro ao editar o usuário!"
+        });
+        return;
+    }
+});
+
+router.delete("/users/:id", async (req: Request, res: Response) => {
+    try {
+
+        const { id } = req.params;
+
+        const userRepository = AppDataSource.getRepository(User);
+
+        const user = await userRepository.findOneBy({ id: parseInt(id) });
+
+        if (!user) {
+            res.status(404).json({
+                message: "Usuário não encontrado!"
+            })
+            return;
+        }
+
+        await userRepository.remove(user);
+
+        res.status(200).json({
+            message: "Usuário apagado com sucesso!",
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message: "Erro ao deletar usuário"
+        });
+    }
+})
 
 
 export default router;
